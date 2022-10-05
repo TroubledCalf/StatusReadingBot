9f7f6241edc94103569951ec7177d156e760ce40
TroubledCalf <46944393+TroubledCalf@users.noreply.github.com>
2022-10-05 19:52:40 -0400

    bot is online!

--- bot.js
+++ bot.js
@@ -1,12 +1,16 @@
-require("dotenv").config(); //to start process from .env file
-const { Client, Intents } = require("discord.js");
-const client = new Client({
-    Intents: [
-        Intents.FLAGS.GUILDS,//adds server functionality
-        Intents.FLAGS.GUILD_MESSAGES //gets messages from our bot.
-    ]
+//require("dotenv").config(); //to start process from .env file
+
+// Require the necessary discord.js classes
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// When the client is ready, run this code (only once)
+client.once('ready', () => {
+	console.log('Ready!');
 });
-client.once("ready", () => {
-    console.log("BOT IS ONLINE"); //message when bot is online
-})
-client.login(process.env.TOKEN);
+
+// Login to Discord with your client's token
+client.login(token);
